dd97eb646ef545a27da7f227a9ed9021280c391a
Andrew Grant <andrew.grant@mail.rit.edu>
2022-05-29 14:33:34 -0400

    simplify prints and pull main into function

--- NLTKStatsModel.py
+++ NLTKStatsModel.py
@@ -75,95 +75,88 @@ def syllable_checker(word):
     return num_syllables
 
 
-# Will build the sentence in a string format
-def build_sentence(sentence):
-    sentence_str = ""
-    for word in sentence:
-        sentence_str = sentence_str + word + " "
-    return sentence_str
-
-
 # Used for debugging syllable output
-def debug_print(syllables, sentence_str):
-    syllable_debug = str(sum(syllables)) + " " + str(syllables)
-    #print(f'{str(syllables):<18}', sum(syllables), "|", sentence_str)
+def debug_print(syllables, sentence):
+    print(f'{str(syllables):<18}', sum(syllables), "|", *sentence)
 
 
 # Creating the haiku
-haiku_created = False
+def create_haiku(first_word, second_word):
+    haiku_created = False
 
-while not haiku_created:
-    sentence = create_sentence("and", "how")
-    sentence = sentence[0:len(sentence) - 2]
-    #print("sentence:", sentence)
+    while not haiku_created:
+        sentence = create_sentence(first_word, second_word)
+        sentence = sentence[0:len(sentence) - 2]
+        #print("sentence:", sentence)
 
-    syllables = []
-    index = 0
+        syllables = []
+        index = 0
 
-    for word in sentence:
-        syllables.append(syllable_checker(word))
-        index += 1
-
-    #print("sylables:", syllables)
-    
-    if sum(syllables) < 17:
-        continue
-    
-    first_sentence = []
-    second_sentence = []
-    third_sentence = []
-    
-    index = 0
-    num_syllables = 0
-    for word in sentence:
-        #print("sylable_num:", num_syllables)
-        if num_syllables >= 5:
-            break
-        elif num_syllables < 5:
-            first_sentence.append(word)
-            num_syllables += syllables[index]
+        for word in sentence:
+            syllables.append(syllable_checker(word))
             index += 1
-            
-    if num_syllables > 5:
-        continue
-    
-    
-    num_syllables = 0
-    for i in range (index, len(sentence)):
-        if num_syllables >= 7:
-            break
-        elif num_syllables < 7:
-            second_sentence.append(sentence[i])
-            num_syllables += syllables[index]
-            index += 1
-    
-    if num_syllables > 7:
-        continue
-            
-    num_syllables = 0
-    for i in range (index, len(sentence)):
-        if num_syllables >= 5:
-            haiku_created = True
-            break
-        elif num_syllables < 5:
-            third_sentence.append(sentence[i])
-            num_syllables += syllables[index]
-            index += 1
-            
-    if num_syllables > 5:
-        continue
 
-    first_sentence_str, second_sentence_str, third_sentence_str = build_sentence(first_sentence), build_sentence(second_sentence), build_sentence(third_sentence)
+        #print("sylables:", syllables)
+        
+        if sum(syllables) < 17:
+            continue
+        
+        first_sentence = []
+        second_sentence = []
+        third_sentence = []
+        
+        index = 0
+        num_syllables = 0
+        for word in sentence:
+            #print("sylable_num:", num_syllables)
+            if num_syllables >= 5:
+                break
+            elif num_syllables < 5:
+                first_sentence.append(word)
+                num_syllables += syllables[index]
+                index += 1
+                
+        if num_syllables > 5:
+            continue
+        
+        
+        num_syllables = 0
+        for i in range (index, len(sentence)):
+            if num_syllables >= 7:
+                break
+            elif num_syllables < 7:
+                second_sentence.append(sentence[i])
+                num_syllables += syllables[index]
+                index += 1
+        
+        if num_syllables > 7:
+            continue
+                
+        num_syllables = 0
+        for i in range (index, len(sentence)):
+            if num_syllables >= 5:
+                haiku_created = True
+                break
+            elif num_syllables < 5:
+                third_sentence.append(sentence[i])
+                num_syllables += syllables[index]
+                index += 1
+                
+        if num_syllables > 5:
+            continue
 
-    first_syllables = syllables[:len(first_sentence)]
-    second_syllables = syllables[len(first_sentence):len(first_sentence + second_sentence)]
-    third_syllables = syllables[len(first_sentence + second_sentence):len(first_sentence + second_sentence + third_sentence)]
+        #first_syllables = syllables[:len(first_sentence)]
+        #second_syllables = syllables[len(first_sentence):len(first_sentence + second_sentence)]
+        #third_syllables = syllables[len(first_sentence + second_sentence):len(first_sentence + second_sentence + third_sentence)]
 
-    #debug_print(first_syllables, first_sentence_str)
-    #debug_print(second_syllables, second_sentence_str)
-    #debug_print(third_syllables, third_sentence_str)
+        #debug_print(first_syllables, first_sentence)
+        #debug_print(second_syllables, second_sentence)
+        #debug_print(third_syllables, third_sentence)
 
-    print(first_sentence_str)
-    print(second_sentence_str)
-    print(third_sentence_str)
+        print(*first_sentence)
+        print(*second_sentence)
+        print(*third_sentence)
 
+
+if __name__ == "__main__":
+    create_haiku('the', 'day')
